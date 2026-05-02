68403ea8574d80f80f4604896bc1d56831924ac7
Yoshi <yoshib4456@outlook.jp>
2026-05-02 22:53:32 +0900

    Add help page

--- kakeibo_project/urls.py
+++ kakeibo_project/urls.py
@@ -22,6 +22,4 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('kakeibo.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
