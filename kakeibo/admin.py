2ef0d3dba43daaeada9f3f0779463cf8e8389f55
Yoshi <yoshib4456@outlook.jp>
2026-05-11 22:45:27 +0900

    added FixedCost

--- kakeibo/admin.py
+++ kakeibo/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Category, Expense
+from .models import FixedCost
 
 admin.site.register(Category)
 admin.site.register(Expense)
+admin.site.register(FixedCost)
