d4336c6a31cb372e2bc94f2cc392aac31330168a
Yoshi <yoshib4456@outlook.jp>
2026-05-13 22:14:39 +0900

    added FixedCost

--- kakeibo/admin.py
+++ kakeibo/admin.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
-from .models import Category, Expense
-from .models import FixedCost
+from .models import Category, Expense, FixedCost
 
-admin.site.register(Category)
-admin.site.register(Expense)
-admin.site.register(FixedCost)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')  # 一覧に表示する項目
+    search_fields = ('name',)        # 名前で検索
 
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'amount', 'category', 'user')
+    list_filter = ('category', 'date')
+    search_fields = ('memo',)
+
+@admin.register(FixedCost)
 class FixedCostAdmin(admin.ModelAdmin):
     list_display = ('name', 'amount', 'category', 'created_at')
     list_filter = ('category',)
     search_fields = ('name',)
+
+
