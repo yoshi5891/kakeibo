f76bac3525566bba5f22696679fad3a13542d99d
Yoshi <yoshib4456@outlook.jp>
2026-05-16 01:26:47 +0900

    family user

--- kakeibo_project/urls.py
+++ kakeibo_project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from kakeibo.views import logout_view
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='kakeibo/login.html'), name='login'),
     path('logout/', logout_view, name='logout'),
+
     path('', include('kakeibo.urls')),
 ]
